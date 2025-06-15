362a62ec35c54ad042754b0a90f06014f559df11
Akashdeep <akashdeep2k4@gmail.com>
2025-06-15 21:48:52 +0530

    add theme definitions for light and dark modes

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  primary: "#FFFFFF",
+  secondary: "#212121",
+  tertiary: "#808080",
+  boxShadow: "0 8px 8px rgba(0, 0, 0, 0.1), 0 -8px 8px rgba(0, 0, 0, 0.1), 8px 0 8px rgba(0, 0, 0, 0.1), -8px 0 8px rgba(0, 0, 0, 0.1);",
+};
+
+export const darkTheme = {
+  primary: "#212121",
+  secondary: "#FFFFFF",
+  tertiary: "#808080",
+  boxShadow: `0 8px 8px rgba(255, 255, 255, 0.1),   
+  0 -8px 8px rgba(255, 255, 255, 0.1), 
+  8px 0 8px rgba(255, 255, 255, 0.1), 
+ -8px 0 8px rgba(255, 255, 255, 0.1);`,
+};
